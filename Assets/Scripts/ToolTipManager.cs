bf9f48b2a0bdb65bdbdf7f69dfaf67dc3a90bfe2
BauRawson <bautistarawson@gmail.com>
2025-03-18 02:22:51 -0300

    - Make sure ToolTip fits screen + add a bit of color

--- Assets/Scripts/ToolTipManager.cs
+++ Assets/Scripts/ToolTipManager.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class ToolTipManager : MonoBehaviour
 {
     [SerializeField] Canvas parentCanvas;
-    [SerializeField] Transform ToolTripTransform;
+    [SerializeField] Transform ToolTipTransform;
     [SerializeField] TMP_Text title, details;
     [SerializeField] RectTransform background;
     [SerializeField] CanvasGroup ToolTipCanvasGroup;
+    [SerializeField] Color plusColor;
+    [SerializeField] Color equipmentColor;
+    [SerializeField] Color consumableColor;
+    [SerializeField] Color enhancerColor;
     bool isShowing;
 
     public static ToolTipManager Instance;
@@ -21,99 +25,145 @@ public class ToolTipManager : MonoBehaviour
 
     private void Update()
     {
-        ToolTripTransform.position = Input.mousePosition;
+        ToolTipTransform.position = Input.mousePosition;
+        AdjustBackgroundPosition();
     }
 
     public void Show(ItemInstance itemInstance)
+{
+    ToolTipCanvasGroup.alpha = 0;
+
+    title.text = itemInstance.itemData.itemName;
+    details.text = itemInstance.itemData.description + "\n\n";
+
+    details.text += "Attack: " + itemInstance.itemData.attack;
+    if (itemInstance.attackModifier != 0)
     {
-        ToolTipCanvasGroup.alpha = 0;
-
-        title.text = itemInstance.itemData.itemName;
-        details.text = itemInstance.itemData.description + "\n\n";
-
-        details.text += "Attack: " + itemInstance.itemData.attack;
-        if (itemInstance.attackModifier != 0)
-        {
-            details.text += "+" + itemInstance.attackModifier + "\n";
-        }
-        else
-        {
-            details.text += "\n";
-        }
-
-        details.text += "Defense: " + itemInstance.itemData.defense;
-        if (itemInstance.defenseModifier != 0)
-        {
-            details.text += "+" + itemInstance.defenseModifier + "\n";
-        }
-        else
-        {
-            details.text += "\n";
-        }
-
-        details.text += "Health: " + itemInstance.itemData.health;
-        if (itemInstance.healthModifier != 0)
-        {
-            details.text += "+" + itemInstance.healthModifier + "\n";
-        }
-        else
-        {
-            details.text += "\n";
-        }
-
-        details.text += "Attack Speed: " + itemInstance.itemData.attackSpeed;
-        if (itemInstance.attackSpeedModifier != 0)
-        {
-            details.text += "+" + itemInstance.attackSpeedModifier + "\n";
-        }
-        else
-        {
-            details.text += "\n";
-        }
-
-        details.text += "Knockback Power: " + itemInstance.itemData.knockbackPower;
-        if (itemInstance.knockbackPowerModifier != 0)
-        {
-            details.text += "+" + itemInstance.knockbackPowerModifier + "\n";
-        }
-        else
-        {
-            details.text += "\n";
-        }
-
-        details.text += "Type: " + itemInstance.itemData.itemType.ToString();
-
-        AdjustBackgroundPosition();
-        ToolTripTransform.gameObject.SetActive(true);
-        isShowing = true;
-        StartCoroutine(FadeIn());
+        details.text += "<color=#" + ColorUtility.ToHtmlStringRGB(plusColor) + ">+" + itemInstance.attackModifier + "</color>\n";
     }
+    else
+    {
+        details.text += "\n";
+    }
+
+    details.text += "Defense: " + itemInstance.itemData.defense;
+    if (itemInstance.defenseModifier != 0)
+    {
+        details.text += "<color=#" + ColorUtility.ToHtmlStringRGB(plusColor) + ">+" + itemInstance.defenseModifier + "</color>\n";
+    }
+    else
+    {
+        details.text += "\n";
+    }
+
+    details.text += "Health: " + itemInstance.itemData.health;
+    if (itemInstance.healthModifier != 0)
+    {
+        details.text += "<color=#" + ColorUtility.ToHtmlStringRGB(plusColor) + ">+" + itemInstance.healthModifier + "</color>\n";
+    }
+    else
+    {
+        details.text += "\n";
+    }
+
+    details.text += "Attack Speed: " + itemInstance.itemData.attackSpeed;
+    if (itemInstance.attackSpeedModifier != 0)
+    {
+        details.text += "<color=#" + ColorUtility.ToHtmlStringRGB(plusColor) + ">+" + itemInstance.attackSpeedModifier + "</color>\n";
+    }
+    else
+    {
+        details.text += "\n";
+    }
+
+    details.text += "Knockback Power: " + itemInstance.itemData.knockbackPower;
+    if (itemInstance.knockbackPowerModifier != 0)
+    {
+        details.text += "<color=#" + ColorUtility.ToHtmlStringRGB(plusColor) + ">+" + itemInstance.knockbackPowerModifier + "</color>\n";
+    }
+    else
+    {
+        details.text += "\n";
+    }
+
+    // Color the item type based on its value
+    Color typeColor;
+    switch (itemInstance.itemData.itemType)
+    {
+        case ItemData.ItemType.Equipment:
+            typeColor = equipmentColor;
+            break;
+        case ItemData.ItemType.Consumable:
+            typeColor = consumableColor;
+            break;
+        case ItemData.ItemType.Enhancer:
+            typeColor = enhancerColor;
+            break;
+        default:
+            typeColor = Color.white;
+            break;
+    }
+
+    details.text += "Type: <color=#" + ColorUtility.ToHtmlStringRGB(typeColor) + ">" + 
+                    itemInstance.itemData.itemType.ToString() + "</color>";
+
+    ToolTipTransform.gameObject.SetActive(true);
+    isShowing = true;
+    StartCoroutine(FadeIn());
+}
 
     private void AdjustBackgroundPosition()
     {
         float offset = 32f;
         Vector2 mousePosition = Input.mousePosition;
         float screenWidth = Screen.width;
-
+        float screenHeight = Screen.height;
+        
+        // Determine if tooltip should be on left or right side based on mouse position
         if (mousePosition.x < screenWidth / 2)
         {
-            background.anchoredPosition = new Vector2(background.sizeDelta.x / 2 + offset, -background.sizeDelta.y / 2 + offset);
+            // Position to the right of the cursor
+            background.anchoredPosition = new Vector2(background.sizeDelta.x / 2 + offset, background.anchoredPosition.y);
         }
         else
         {
-            background.anchoredPosition = new Vector2(-background.sizeDelta.x / 2 - offset, -background.sizeDelta.y / 2 + offset);
+            // Position to the left of the cursor
+            background.anchoredPosition = new Vector2(-background.sizeDelta.x / 2 - offset, background.anchoredPosition.y);
         }
+        
+        // Calculate vertical position
+        float tooltipHeight = background.rect.height;
+        float halfTooltipHeight = tooltipHeight / 2;
+        
+        // Default vertical position
+        float verticalPosition = mousePosition.y;
+        
+        // Check if tooltip would go off the top of the screen
+        if (mousePosition.y + halfTooltipHeight > screenHeight)
+        {
+            // Snap to top of screen
+            verticalPosition = screenHeight - halfTooltipHeight;
+        }
+        // Check if tooltip would go off the bottom of the screen
+        else if (mousePosition.y - halfTooltipHeight < 0)
+        {
+            // Snap to bottom of screen
+            verticalPosition = halfTooltipHeight;
+        }
+        
+        // Apply vertical position
+        background.position = new Vector3(background.position.x, verticalPosition, background.position.z);
     }
 
     public void Hide()
     {
-        ToolTripTransform.gameObject.SetActive(false);
+        ToolTipTransform.gameObject.SetActive(false);
         isShowing = false;
     }
 
     private IEnumerator FadeIn()
     {
-        float duration = 0.5f;
+        float duration = 1.0f;
         float counter = 0;
 
         while (counter < duration)
